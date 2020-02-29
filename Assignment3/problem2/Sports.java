322190810c674875a19fe81f9f310727686efae8
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-02-29 00:10:37 -0600

    Update Sports.java

--- Assignment3/problem2/Sports.java
+++ Assignment3/problem2/Sports.java
@@ -1,10 +1,17 @@
-public abstract class Sports extends Cars {
+final class Sports extends Cars {
 	// instance variables
-	protected int raceStats;
+	private int raceStats;
 	
 	// constructors
-	public Sports(int make, int model, int vinNumber, String carType) {
+	public Sports(String make, String model, int vinNumber, String carType, int raceStats) {
 		super (make, model, vinNumber, carType);
 		this.raceStats = raceStats;
 	}
+	@Override
+	public String toString() {
+		String sportscarinfo = "\nCar information: " + "\nVin Number: " + vinNumber + "\nCar Type: " + carType + "\nMake: " + make + 
+				"\nModel: " + model + "\nRace Stats: " + raceStats;
+		return sportscarinfo;
+	}
 }
+
