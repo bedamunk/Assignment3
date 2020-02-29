1236b5974ef73c591debc8952a92b933825ec9fa
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-02-29 00:09:51 -0600

    Update Sedan.java

--- Assignment3/problem2/Sedan.java
+++ Assignment3/problem2/Sedan.java
@@ -1,14 +1,19 @@
-
-public abstract class Sedan extends Cars {
+public final class Sedan extends Cars {
 	// instance variables
-	protected int numDoors;
-	protected int trunkSize;
+	private int numDoors;
+	private int trunkSize;
 	
 	// constructors
-	public Sedan(int make, int model, int vinNumber, String carType) {
+	public Sedan(String make, String model, int vinNumber, String carType, int numDoors, int trunkSize) {
 		super (make, model, vinNumber, carType);
 		this.numDoors = numDoors;
 		this.trunkSize = trunkSize;
+	}
+	@Override
+	public String toString() {
+		String sedancarinfo = "\nCar information: " + "\nVin Number: " + vinNumber + "\nCar Type: " + carType + "\nMake: " + make + 
+				"\nModel: " + model + "\nNumber of Doors: " + numDoors + "\nTrunk Size: " + trunkSize;
+		return sedancarinfo;
 	}
 }
 
