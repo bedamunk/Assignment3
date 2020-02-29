28daf98b52845bfda13ff39a9946066a6fd7431f
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-02-29 00:09:10 -0600

    Update Cars.java

--- Assignment3/problem2/Cars.java
+++ Assignment3/problem2/Cars.java
@@ -1,6 +1,5 @@
-
 public class Cars {
-	package problem2;
+	
 
 	/*
 	 * @author Red Team
@@ -11,43 +10,30 @@ public class Cars {
 	 * Bethanie Munkres
 	 */
 
-	public class Cars {
-
 		// private instance variables
-		protected int make;
-		protected int model;
+		protected String make;
+		protected String model;
 		protected int vinNumber;
 		
-		// privaate instance variables with aggregation type relationship
-		protected String carType
+		// private instance variables with aggregation type relationship
+		protected String carType;
 		
 		// constructor takes make model vinNumber and carType
-		public Cars(int make, int model, int vinNumber, String carType) {
+		public Cars(String make, String model, int vinNumber, String carType) {
 			// set variables equal to the user input
 			this.make = make;
 			this.model = model;
 			this.vinNumber = vinNumber;
-			/* testing note:
-			 * REMOVE THIS NOTE BEFORE FINALIZING*******************************
-			 * this if test will likely be thrown out.  Its a reminder to check
-			 * for any conflict til I know how this is being input.  
-			 * Feel free to remove if you know when you read this
-			 * *****************************************************************
-			 */
-			// LINES IN QUESTION BEGINNING
-			if(carType == null)
-				this.carType = neither;		
-			// LINIES IN QUESTION END
 			this.carType = carType;
 		}
 		// setters**************************************************************
 		
 		// setter method to set private data field make
-		public void setMake(int newMake) {
+		public void setMake(String newMake) {
 			this.make = newMake;
 		}
 		// setter method to set private data field model
-		public void setModel(int newModel) {
+		public void setModel(String newModel) {
 			this.model = newModel;
 				}
 		// setter method to set private data field vinNumber
@@ -63,11 +49,11 @@ public class Cars {
 		// getters**************************************************************
 		
 		// getter method to return private data field make
-		public int getMake() {
+		public String getMake() {
 			return this.make;
 		}
 		// getter method to return private data field model
-		public int getModel() {
+		public String getModel() {
 			return this.model;
 		}
 		// getter method to return private data field vinNumber
@@ -78,20 +64,12 @@ public class Cars {
 		public String getCarType() {
 			return this.carType;
 		}
-
-		/* testing note:
-		 * REMOVE THIS NOTE BEFORE FINALIZING*******************************
-		 * Unsure about final state of toString()
-		 * leaving note to check info given/format or lack of need for toString()
-		 * *****************************************************************
-		 */
+		
 		// toString method
 		public String toString() {
-			String carinfo = "\nCar information: /nVin Number: " + vinNumber + "/nMake: " + make + 
-					"/nModel: " + model;
+			String carinfo = "\nCar information: /nVin Number: " + vinNumber + "/nCar Type: " + carType + "/nMake: " + make + 
+			"/nModel: " + model;
 			return carinfo;
 		}
-	}
-
-
+	
 }
