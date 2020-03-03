e36e3e986d92c54ed70fc6943a1bbb294e265396
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-03-03 00:00:55 -0600

    Update Sedan.java
    made changes relating to removal of carType variable from all classes

--- Assignment3/problem2/Sedan.java
+++ Assignment3/problem2/Sedan.java
@@ -1,18 +1,19 @@
+
 public final class Sedan extends Cars {
+	
 	// instance variables
 	private int numDoors;
 	private int trunkSize;
 	
 	// constructors
-	public Sedan(String make, String model, int vinNumber, String carType, int numDoors, int trunkSize) {
-		super (make, model, vinNumber, carType);
+	public Sedan(String make, String model, int vinNumber, int numDoors, int trunkSize, CarOwners owner) {
+		super (make, model, vinNumber,owner);
 		this.numDoors = numDoors;
 		this.trunkSize = trunkSize;
 	}
 	@Override
 	public String toString() {
-		String sedancarinfo = "\nCar information: " + "\nVin Number: " + vinNumber + "\nCar Type: " + carType + "\nMake: " + make + 
-				"\nModel: " + model + "\nNumber of Doors: " + numDoors + "\nTrunk Size: " + trunkSize;
+		String sedancarinfo = super.toString() + "\nNumber of Doors: " + numDoors + "\nTrunk Size: " + trunkSize;
 		return sedancarinfo;
 	}
 }
