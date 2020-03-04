24e02eb535c0cf58779a7febf982ca2df8c69627
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-03-04 00:55:37 -0600

    Update Sedan.java

--- Assignment3/problem2/Sedan.java
+++ Assignment3/problem2/Sedan.java
@@ -1,9 +1,14 @@
+package problem2;
 
-public final class Sedan extends Cars {
+final class Sedan extends Cars {
 	
 	// instance variables
 	private int numDoors;
 	private int trunkSize;
+	
+	public Sedan() {
+		
+	}
 	
 	// constructors
 	public Sedan(String make, String model, int vinNumber, int numDoors, int trunkSize, CarOwners owner) {
@@ -11,6 +16,31 @@ public final class Sedan extends Cars {
 		this.numDoors = numDoors;
 		this.trunkSize = trunkSize;
 	}
+	
+	// setter method to set protected data field numDoors
+				public void setNumDoors(int numDoors) {
+					this.numDoors = numDoors;
+				}
+				
+				
+				
+		// getter method to return private data field numDoors
+				public int getNumDoors() {
+					return this.numDoors;
+				}
+				
+				
+				// setter method to set protected data field trunkSize
+				public void setTrunkSize(int trunkSize) {
+					this.trunkSize = trunkSize;
+				}
+				
+				
+				
+		// getter method to return private data field trunkSize
+				public int getTrunkSize() {
+					return this.trunkSize;
+				}		
 	@Override
 	public String toString() {
 		String sedancarinfo = super.toString() + "\nNumber of Doors: " + numDoors + "\nTrunk Size: " + trunkSize;
